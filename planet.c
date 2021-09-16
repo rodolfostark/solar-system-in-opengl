7678600e6ec3c8b2a3b34df2fc5f82d7da1912f9
rodolfostark <rodolfostark@ufrn.edu.br>
2021-09-15 21:39:16 -0300

    bora

--- planet.c
+++ planet.c
@@ -1,6 +1,8 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 static int year = 0, day = 0;
 
@@ -24,15 +26,18 @@ float mercury_distance = earth_distance * 0.39,
 
 void init(void)
 {
-  glClearColor(0.0, 0.0, 0.0, 0.0);
+  glClearColor(0.38, 0.4, 0.53, 0.21);
   glShadeModel(GL_FLAT);
+  glEnable(GL_TEXTURE_2D);
 }
 
-void stars(void) {
+void stars(void)
+{
   int noOfStars = rand() % 10;
   int i = 0;
 
-  while (i < noOfStars) {
+  while (i < noOfStars)
+  {
     glColor3f(0.0, 1.0, 1.0);
     glPointSize(20.0f);
 
@@ -46,25 +51,50 @@ void stars(void) {
 
     i++;
   }
-
   glFlush();
   glutSwapBuffers();
 }
 
+void make_texture(GLUquadricObj *planet, const char* filename)
+{
+  // Carregando imagem de textura do Sol
+  GLuint tex;
+  unsigned char data[256][256][3];
+  FILE *file;
+
+  // open texture data
+  file = fopen(filename, "rb");
+  if (file == NULL)
+    return 0;
+  fread(data, 256 * 256 * 3, 1, file);
+  fclose(file);
+
+  glGenTextures(1, &tex);
+  glBindTexture(GL_TEXTURE_2D, tex);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 256, 256, 0, GL_RGB, GL_UNSIGNED_BYTE, (const GLvoid *) data);
+  
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  
+  gluQuadricDrawStyle(planet, GLU_FILL);
+  glBindTexture(GL_TEXTURE_2D, tex);
+  gluQuadricTexture(planet, GL_TRUE);
+}
+
 void renderPlanet(GLUquadricObj *planet, float size, float translateX,
                   float translateY, float translateZ, float rotationAngle,
-                  float rotationX, float rotationY, float rotationZ, float yearRateFromEarth) 
+                  float rotationX, float rotationY, float rotationZ, float yearRateFromEarth, const char* filename)
 {
   glPopMatrix();
   glPushMatrix();
 
-  glRotatef((GLfloat) year * yearRateFromEarth, 0.0, 1.0, 0.0);
+  glRotatef((GLfloat)year * yearRateFromEarth, 0.0, 1.0, 0.0);
   glTranslatef(translateX, translateY, translateZ);
-  glRotatef((GLfloat) year * yearRateFromEarth * rotationAngle, rotationX, rotationY, rotationZ);
-  gluSphere(planet, size, 100, 80); /* draw mars planet */
+  glRotatef((GLfloat)year * yearRateFromEarth * rotationAngle, rotationX, rotationY, rotationZ);
+  gluSphere(planet, size, 200, 200);
 }
 
-void displayFirstSet(void) 
+void displayFirstSet(void)
 {
   // stars();
   GLUquadricObj *sun = NULL;
@@ -92,29 +122,29 @@ void displayFirstSet(void)
 
   glClear(GL_COLOR_BUFFER_BIT);
   glColor3f(1.0, 1.0, 1.0);
-
   glPushMatrix();
 
   gluSphere(sun, 1.0, 200, 160);           /* draw sun */
-  glRotatef((GLfloat) year, 0.0, 1.0, 0.0); // rotate sun
-  renderPlanet(mercury, mercury_size, mercury_distance, 1.0, 0.0, 2, 0.0, 1.0, 0.0, 4.0);
-  renderPlanet(venus, venus_size, venus_distance, 1.0, 0.0, 2, 0.0, 1.0, 0.0, 1.6);
-  renderPlanet(earth, earth_size, earth_distance, 1.0, 0.0, 2, 0.0, 1.0, 0.0, 1.0);
+  glRotatef((GLfloat)year, 0.0, 1.0, 0.0); // rotate sun
+
+  renderPlanet(mercury, mercury_size, mercury_distance, 1.0, 0.0, 2, 0.0, 1.0, 0.0, 4.0, "./2k_earth_daymap.jpg");
+  renderPlanet(venus, venus_size, venus_distance, 1.0, 0.0, 2, 0.0, 1.0, 0.0, 1.6, "./2k_earth_daymap.jpg");
+  renderPlanet(earth, earth_size, earth_distance, 1.0, 0.0, 2, 0.0, 1.0, 0.0, 1.0, "./2k_earth_daymap.jpg");
   // **** Render Mars****
-  renderPlanet(mars, mars_size, mars_distance, 1.0, 0.0, 2, 0.0, 1.0, 0.0, 0.5);
-  renderPlanet(jupiter, jupiter_size, jupiter_distance, 1.0, 0.0, 2, 0.0, 1.0, 0.0, 0.08);
+  renderPlanet(mars, mars_size, mars_distance, 1.0, 0.0, 2, 0.0, 1.0, 0.0, 0.5, "./2k_earth_daymap.jpg");
+  renderPlanet(jupiter, jupiter_size, jupiter_distance, 1.0, 0.0, 2, 0.0, 1.0, 0.0, 0.08, "./2k_earth_daymap.jpg");
   renderPlanet(saturn, saturn_size, saturn_distance, 1.0, 0.0, 2, 0.0, 1.0,
-               0.0, 0.03);
+               0.0, 0.03, "./2k_earth_daymap.jpg");
   renderPlanet(uranus, uranus_size, uranus_distance, 1.0, 0.0, 2, 0.0, 1.0,
-               0.0, 0.01);
+               0.0, 0.01, "./2k_earth_daymap.jpg");
   renderPlanet(neptune, neptune_size, neptune_distance, 1.0, 0.0, 2, 0.0, 1.0,
-               0.0, 0.006);
-  renderPlanet(pluto, pluto_size, pluto_distance, 1.0, 0.0, 2, 0.0, 1.0, 0.0, 0.004);
+               0.0, 0.006, "./2k_earth_daymap.jpg");
+  renderPlanet(pluto, pluto_size, pluto_distance, 1.0, 0.0, 2, 0.0, 1.0, 0.0, 0.004, "./2k_earth_daymap.jpg");
   glPopMatrix();
   glutSwapBuffers();
 }
 
-void reshape(int w, int h) 
+void reshape(int w, int h)
 {
   glViewport(0, 0, (GLsizei)w, (GLsizei)h);
   glMatrixMode(GL_PROJECTION);
@@ -122,11 +152,13 @@ void reshape(int w, int h)
   gluPerspective(120.0, (GLfloat)w / (GLfloat)h, 2.0, 550.0);
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-  gluLookAt(6.0, 6.0, 6.0, 0.0, 3.0, 0.0, 0.0, 1.0, 0.0);
+  gluLookAt(4.0, 4.0, 4.0, 0.0, 3.0, 0.0, 0.0, 1.0, 0.0);
 }
 
-void keyboard(unsigned char key, int x, int y) {
-  switch (key) {
+void keyboard(unsigned char key, int x, int y)
+{
+  switch (key)
+  {
   case 'd':
     day = (day + 10) % 360;
     glutPostRedisplay();
@@ -155,7 +187,7 @@ void idle()
   year = (year + 5);
 }
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
